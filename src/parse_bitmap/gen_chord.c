d9c679117cac785b2716a4031d3405ef4a515573
jakew-0-0 <jakewbailey007@gmail.com>
2023-07-07 19:49:36 +0100

    stuff

--- src/parse_bitmap/gen_chord.c
+++
@@ -1,6 +0,0 @@
-#include "parse_bmp.h"
-
-
-void gen_chord(float* chord_pool, int size, int sub_size, char* path) {
-
-}
--- src/parse_bitmap/gen_chords.c
+++
@@ -1,5 +0,0 @@
-#include "parse_bmp.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-
